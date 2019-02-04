65d84ff35a214deffcaff42aa15d9df5d07ae83a
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-02-04 23:55:40 +0900

    init

---
+++ build/precache-manifest.20548844cee84e1abe0016fa9478b9c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "17f4912f0893c4e873df",
+    "url": "/static/js/main.17f4912f.chunk.js"
+  },
+  {
+    "revision": "e1aa8b9da398c9d8125b",
+    "url": "/static/js/1.e1aa8b9d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc1a5b93b24490c7379292f024e950b8",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import React, { FunctionComponent, useEffect } from "react";
+import useObject from "useobject";
+import axios from "axios";
+import PieChart from "./components/pie";
+
+const App: FunctionComponent = () => {
+  const req = axios.create({ baseURL: "https://api.github.com/users/" });
+
+  const { state, setState } = useObject({
+    result: [] as { type: string; count: number }[],
+    input: ""
+  });
+
+  const fetch = async (user: string) => {
+    let ress: any[] = [];
+    for (let i = 0; ; i++) {
+      const res = await req
+        .get(user + "/repos?per_page=100&page=" + i)
+        .catch(console.log);
+      if ((res as any).data.length === 0) break;
+      if (!res) break;
+      ress.push((res as any).data);
+    }
+    const res = ress.flatMap(item => item);
+    console.log({ res });
+    if (res.length === 0) return;
+
+    const languages = res.flatMap(item => {
+      if (item.language) {
+        return [item.language];
+      } else {
+        return [];
+      }
+    });
+
+    const results: { type: string; count: number }[] = [];
+    const map: { [key: string]: number } = {};
+    languages.forEach(lang => {
+      if (Object.keys(map).includes(lang)) {
+        map[lang]++;
+      } else {
+        map[lang] = 1;
+      }
+    });
+    Object.keys(map).map(key => results.push({ type: key, count: map[key] }));
+    setState({
+      result: results
+    });
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="user name"
+        onChange={e => setState({ input: e.target.value })}
+        value={state.input}
+      />
+      <button
+        onClick={() => {
+          fetch(state.input);
+          setState({ input: "" });
+        }}
+      >
+        show
+      </button>
+      {state.result.length > 0 && (
+        <div style={{ display: "flex" }}>
+          <div>
+            {state.result
+              .sort((a, b) => b.count - a.count)
+              .map(item => (
+                <p key={item.type}>
+                  {item.type}:{item.count}
+                </p>
+              ))}
+          </div>
+          <PieChart data={state.result} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
