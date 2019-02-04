355de4f31ca2b0d236d9b2c3257e94447d18f601
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-02-04 23:58:02 +0900

    fix build

---
+++ build/precache-manifest.5b6826f487482de5acc4c86fdf170806.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "934e0ad9602c0107cf0e",
+    "url": "./static/js/main.934e0ad9.chunk.js"
+  },
+  {
+    "revision": "e1aa8b9da398c9d8125b",
+    "url": "./static/js/1.e1aa8b9d.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "dac6785f94250737e2e1af7f2431f52c",
+    "url": "./index.html"
+  }
+];
